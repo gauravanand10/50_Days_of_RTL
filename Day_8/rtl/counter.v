982640108861e6cf6568bc4f4a24a8766e1a1129
gauravanand10 <ganand_be23@thapar.edu>
2026-06-26 22:04:04 +0530

    Create counter.v

---
+++ Day_8/rtl/counter.v
@@ -0,0 +1,15 @@
+module counter #(parameter WIDTH = 4)(
+    input clk,
+    input rst,
+    output reg [WIDTH-1:0] count
+);
+
+    always @(posedge clk or posedge rst)
+    begin
+        if (rst)
+            count <= 0;
+        else
+            count <= count + 1;
+    end
+
+endmodule
