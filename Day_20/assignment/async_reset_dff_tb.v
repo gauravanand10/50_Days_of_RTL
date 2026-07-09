c115fa4a4601a28cc91151486ee1e7a413d6cd7a
gauravanand10 <ganand_be23@thapar.edu>
2026-07-09 18:41:35 +0530

    Create async_reset_dff_tb.v

---
+++ Day_20/assignment/async_reset_dff_tb.v
@@ -0,0 +1,52 @@
+`timescale 1ns/1ps
+
+module async_reset_dff_tb;
+
+    reg clk;
+    reg rst;
+
+    wire q;
+
+    async_reset_dff uut(
+
+        .clk(clk),
+        .rst(rst),
+        .q(q)
+
+    );
+
+    initial
+        clk = 0;
+
+    always #5
+        clk = ~clk;
+
+    initial begin
+
+        rst = 1;
+
+        #15 rst = 0;
+
+        #30 rst = 1;
+
+        #10 rst = 0;
+
+        #30 rst = 1;
+
+        #10 rst = 0;
+
+        #20 $finish;
+
+    end
+
+    initial begin
+
+        $monitor("Time=%0t clk=%b rst=%b d=%b q=%b",
+                  $time, clk, rst, uut.d, q);
+
+        $dumpfile("wave.vcd");
+        $dumpvars(0, async_reset_dff_tb);
+
+    end
+
+endmodule
