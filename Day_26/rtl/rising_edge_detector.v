325e988fb6a6b479d6458107d4630c25769742cb
gauravanand10 <ganand_be23@thapar.edu>
2026-07-16 16:00:31 +0000

    Create rising_edge_detector.v

---
+++ Day_26/rtl/rising_edge_detector.v
@@ -0,0 +1,20 @@
+module rising_edge_detector(
+
+    input clk,
+    input signal_in,
+
+    output reg edge_detected
+
+);
+
+    reg signal_prev;
+
+    always @(posedge clk) begin
+
+        edge_detected <= ~signal_prev & signal_in;
+
+        signal_prev <= signal_in;
+
+    end
+
+endmodule
