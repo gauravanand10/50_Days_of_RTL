c394448ac0fa8aaf8151ccaa80dab4b8943c3d55
gauravanand10 <ganand_be23@thapar.edu>
2026-07-16 16:01:01 +0000

    Create rising_edge_detector_tb.v

---
+++ Day_26/tb/rising_edge_detector_tb.v
@@ -0,0 +1,52 @@
+`timescale 1ns/1ps
+
+module rising_edge_detector_tb;
+
+    reg clk;
+    reg signal_in;
+
+    wire edge_detected;
+
+    rising_edge_detector uut(
+
+        .clk(clk),
+        .signal_in(signal_in),
+        .edge_detected(edge_detected)
+
+    );
+
+    initial
+        clk = 0;
+
+    always #5
+        clk = ~clk;
+
+    initial begin
+
+        signal_in = 0;
+
+        #12 signal_in = 1;
+
+        #10 signal_in = 0;
+
+        #10 signal_in = 1;
+
+        #10 signal_in = 1;
+
+        #10 signal_in = 0;
+
+        #10 $finish;
+
+    end
+
+    initial begin
+
+        $monitor("Time=%0t clk=%b signal_in=%b edge_detected=%b",
+                 $time, clk, signal_in, edge_detected);
+
+        $dumpfile("wave.vcd");
+        $dumpvars(0, rising_edge_detector_tb);
+
+    end
+
+endmodule
