b3de4b4b01b5ce7525113773c8067c68bf898eca
gauravanand10 <ganand_be23@thapar.edu>
2026-07-17 12:21:53 +0000

    Create dual_edge_detector_tb.v

---
+++ Day_26/simulation/dual_edge_detector_tb.v
@@ -0,0 +1,53 @@
+`timescale 1ns/1ps
+
+module dual_edge_detector_tb;
+
+    reg clk;
+    reg signal_in;
+
+    wire edge_detected;
+
+    dual_edge_detector uut(
+
+        .clk(clk),
+        .signal_in(signal_in),
+        .edge_detected(edge_detected)
+
+    );
+
+    initial
+        clk = 0;
+
+    always #5
+        clk = ~clk;
+
+    initial begin
+
+        signal_in = 0;
+
+        #12 signal_in = 1;
+
+        #10 signal_in = 0;
+
+        #10 signal_in = 1;
+
+        #10 signal_in = 1;
+
+        #10 signal_in = 0;
+
+        #10 $finish;
+
+    end
+
+    initial begin
+
+        $monitor("Time=%0t clk=%b signal_in=%b edge_detected=%b",
+                 $time, clk, signal_in, edge_detected);
+
+        $dumpfile("wave.vcd");
+        $dumpvars(0, dual_edge_detector_tb);
+
+    end
+
+endmodule
+
