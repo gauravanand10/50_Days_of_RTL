b4e0e20a6c22efdc33035bbe0902b88f0e6718df
gauravanand10 <ganand_be23@thapar.edu>
2026-07-19 13:34:53 +0000

    Create priority_arbiter.v

---
+++ Day_27/assignment/priority_arbiter.v
@@ -0,0 +1,31 @@
+module priority_arbiter(
+
+    input clk,
+    input reset,
+    input [1:0] request,
+
+    output reg [1:0] grant
+
+);
+
+always @(posedge clk or posedge reset) begin
+
+    if(reset)
+        grant <= 2'b00;
+
+    else begin
+
+        if(request[0])
+            grant <= 2'b10;
+
+        else if(request[1])
+            grant <= 2'b01;
+
+        else
+            grant <= 2'b00;
+
+    end
+
+end
+
+endmodule
