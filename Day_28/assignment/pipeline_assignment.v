013f83041ce7b477f0c11f5a7620641361f36905
gauravanand10 <ganand_be23@thapar.edu>
2026-07-22 15:16:33 +0000

    Create pipeline_assignment.v

---
+++ Day_28/assignment/pipeline_assignment.v
@@ -0,0 +1,43 @@
+module pipeline_assignment(
+
+    input clk,
+    input reset,
+    input [15:0] a,
+    input [15:0] b,
+    input [15:0] c,
+    input [15:0] d,
+
+    output reg [15:0] result
+
+);
+
+    reg [15:0] stage1_reg;
+    reg [15:0] stage2_reg;
+    reg [15:0] stage3_reg;
+
+    always @(posedge clk or posedge reset) begin
+
+        if(reset) begin
+
+            stage1_reg <= 16'd0;
+            stage2_reg <= 16'd0;
+            stage3_reg <= 16'd0;
+            result <= 16'd0;
+
+        end
+
+        else begin
+
+            stage1_reg <= a + b;
+
+            stage2_reg <= stage1_reg * c;
+
+            stage3_reg <= stage2_reg - d;
+
+            result <= stage3_reg;
+
+        end
+
+    end
+
+endmodule
