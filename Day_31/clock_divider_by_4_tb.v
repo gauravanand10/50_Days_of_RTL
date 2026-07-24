a86ac7a58594ceb765e957e85ca08cd64178810c
gauravanand10 <ganand_be23@thapar.edu>
2026-07-24 17:17:11 +0000

    Create clock_divider_by_4_tb.v

---
+++ Day_31/clock_divider_by_4_tb.v
@@ -0,0 +1,43 @@
+`timescale 1ns/1ps
+
+module clock_divider_by_4_tb;
+
+    reg clk_in;
+    reg reset;
+    wire clk_out;
+
+    clock_divider_by_4 uut (
+        .clk_in(clk_in),
+        .reset(reset),
+        .clk_out(clk_out)
+    );
+
+    initial begin
+        clk_in = 0;
+        forever #5 clk_in = ~clk_in;
+    end
+
+    initial begin
+        reset = 1;
+        #20;
+        reset = 0;
+        #120;
+        $finish;
+    end
+
+    initial begin
+        $dumpfile("wave.vcd");
+        $dumpvars(0, clock_divider_by_4_tb);
+    end
+
+    initial begin
+        $display("Time\tReset\tCounter\tclk_in\tclk_out");
+        $monitor("%0t\t%b\t%d\t%b\t%b",
+                 $time,
+                 reset,
+                 uut.counter,
+                 clk_in,
+                 clk_out);
+    end
+
+endmodule
